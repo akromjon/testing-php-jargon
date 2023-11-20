a0ffc7cf4376266b0235f022d81461fed5704f22
akromjon <numonov.akromjon1998@gmail.com>
2023-11-20 17:54:15 +0500

    fix: setup method to TagParseTest.php file

--- tests/TagParseTest.php
+++ tests/TagParseTest.php
@@ -4,23 +4,26 @@ use Akromjon\TestingPhpJargon\TagParser;
 
 class TagParseTest extends \PHPUnit\Framework\TestCase
 {
-    public function test_it_parses_without_spaces_of_tags(){
-        $tagParser = new TagParser();
-        $result1 = $tagParser->parse('some,tags');
-        $this->assertSame(['some','tags'], $result1);
+    protected TagParser $tagParser;
+    public function setUp():void
+    {
+        $this->tagParser = new TagParser();
     }
-    public function test_it_parses_with_comma_of_tags(){
-
-        $tagParser = new TagParser();
-        $result1 = $tagParser->parse('some, input');
-        $this->assertSame(['some','input'], $result1);      
+    public function test_it_parses_without_spaces_of_tags()
+    {
+        $parser = $this->tagParser->parse('some,tags');
+        $this->assertSame(['some','tags'], $parser);
+    }
+    public function test_it_parses_with_comma_of_tags()
+    {
+        $parser = $this->tagParser->parse('some, input');
+        $this->assertSame(['some','input'], $parser);      
     }
     
-    public function test_it_parses_with_pipe_of_tags(){
-            
-        $tagParser = new TagParser();
-        $result1 = $tagParser->parse('some | input');
-        $this->assertSame(['some','input'], $result1);    
+    public function test_it_parses_with_pipe_of_tags()
+    {            
+        $parser = $this->tagParser->parse('some | input');
+        $this->assertSame(['some','input'], $parser);    
             
     }
 }
