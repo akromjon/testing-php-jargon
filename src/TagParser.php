681a9c219b579590b784b07d61e33a75d932539c
akromjon <numonov.akromjon1998@gmail.com>
2023-11-20 17:48:43 +0500

    feature: it parses: strings with spaces and pipes

--- src/TagParser.php
+++ src/TagParser.php
@@ -2,7 +2,8 @@
 
 namespace Akromjon\TestingPhpJargon;
 class TagParser{
-    public function parse(string $tags):array{
-        return explode(',', $tags);
+    public function parse(string $tags):array
+    {
+        return preg_split("/ ?[,|] ?/", $tags);        
     }
 }
--- tests/TagParseTest.php
+++ tests/TagParseTest.php
@@ -4,13 +4,23 @@ use Akromjon\TestingPhpJargon\TagParser;
 
 class TagParseTest extends \PHPUnit\Framework\TestCase
 {
-    public function testParse(){
+    public function test_it_parses_without_spaces_of_tags(){
+        $tagParser = new TagParser();
+        $result1 = $tagParser->parse('some,tags');
+        $this->assertSame(['some','tags'], $result1);
+    }
+    public function test_it_parses_with_comma_of_tags(){
 
         $tagParser = new TagParser();
-        $result1 = $tagParser->parse(' some,input');
-        $this->assertSame(['some','input'], $result1);
-
-        $result2 = $tagParser->parse('some, input');
-        $this->assertSame(['some','input'], $result2);
-    }    
+        $result1 = $tagParser->parse('some, input');
+        $this->assertSame(['some','input'], $result1);      
+    }
+    
+    public function test_it_parses_with_pipe_of_tags(){
+            
+        $tagParser = new TagParser();
+        $result1 = $tagParser->parse('some | input');
+        $this->assertSame(['some','input'], $result1);    
+            
+    }
 }
