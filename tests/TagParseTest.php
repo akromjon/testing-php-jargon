d4dca6825cd3fd75ed760d5b43f3ea6fbc6ec39a
akromjon <numonov.akromjon1998@gmail.com>
2023-11-20 20:36:59 +0500

    fix: @dataProvider has been added tagsProdiver

--- tests/TagParseTest.php
+++ tests/TagParseTest.php
@@ -3,27 +3,28 @@
 use Akromjon\TestingPhpJargon\TagParser;
 
 class TagParseTest extends \PHPUnit\Framework\TestCase
-{
-    protected TagParser $tagParser;
-    public function setUp():void
+{ 
+
+    /**
+     * @test
+     * @dataProvider tagsProvider
+     */
+    public function test_it_parses_tags($input,$expected)
     {
-        $this->tagParser = new TagParser();
-    }
-    public function test_it_parses_without_spaces_of_tags()
-    {
-        $parser = $this->tagParser->parse('some,tags');
-        $this->assertSame(['some','tags'], $parser);
-    }
-    public function test_it_parses_with_comma_of_tags()
-    {
-        $parser = $this->tagParser->parse('some, input');
-        $this->assertSame(['some','input'], $parser);      
+        $parser = (new TagParser)->parse($input);
+        $this->assertSame($expected, $parser);
     }
     
-    public function test_it_parses_with_pipe_of_tags()
-    {            
-        $parser = $this->tagParser->parse('some | input');
-        $this->assertSame(['some','input'], $parser);    
-            
+    public  static function tagsProvider():array
+    {
+        return [
+            ['some,tags',['some','tags']],
+            ['some | input',['some','input']],
+            ['some | input,tags',['some','input','tags']],
+            ['some | input, tags | here',['some','input','tags','here']],
+            ['some | input, tags | here, more | input',['some','input','tags','here','more','input']],
+        ];
     }
+
+    
 }
